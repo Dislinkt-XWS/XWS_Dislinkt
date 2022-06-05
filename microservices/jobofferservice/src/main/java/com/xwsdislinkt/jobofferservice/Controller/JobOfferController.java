9a8a1147ceef38cb4fab33eaf7205f299834ebcd
Boris Horvat <horvat.boris99@gmail.com>
2022-06-05 14:32:19 +0200

    feat: sending job offers to dislinkt

--- microservices/jobofferservice/src/main/java/com/xwsdislinkt/jobofferservice/Controller/JobOfferController.java
+++ microservices/jobofferservice/src/main/java/com/xwsdislinkt/jobofferservice/Controller/JobOfferController.java
@@ -1,7 +1,11 @@
 package com.xwsdislinkt.jobofferservice.Controller;
 
+import com.xwsdislinkt.jobofferservice.DTO.JobOfferDTO;
 import com.xwsdislinkt.jobofferservice.Service.JobOfferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobOfferController {
     @Autowired
     JobOfferService jobOfferService;
+
+    @PostMapping
+    public ResponseEntity<JobOfferDTO> postedFromAgent(){
+        System.out.println("Got pinged from agent api");
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
